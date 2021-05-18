b8bb0c18ca944f3b2f33bac7131ced96e6e4ae41
Hilina <hhats2013@gmail.com>
2021-05-18 03:43:44 +0300

    Update febonaci.js

--- computation/febonaci.js
+++ computation/febonaci.js
@@ -1,11 +1,3 @@
-// const fibonacci = function (num) {
-//   if (num <= 2) {
-//     return 1;
-//   } else {
-//     return fibonacci(num - 1) + fibonacci(num - 2);
-//   }
-// };
-// console.log("Fibonacci of 10 is " + fibonacci(10));
 
 function fibonacci(num) {
   if (num <= 2) {
