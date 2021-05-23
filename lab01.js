426d9d076602f707ad487ad7a6a236bb057cdeab
Hilina <hhats2013@gmail.com>
2021-05-24 02:40:45 +0300

    Update lab01.js

--- lab01.js
+++ lab01.js
@@ -7,13 +7,5 @@ console.log("1");
 const newProcess = child.spawn("node", ["./computation/febonaci"], {
   stdio: "inherit",
 });
-const newProcess2 = child.spawn("node", ["./computation/febonaci"], {
-  stdio: "inherit",
-});
-const newProcess3 = child.spawn("node", ["./computation/febonaci"], {
-  stdio: "inherit",
-});
-const newProcess4 = child.spawn("node", ["./computation/febonaci"], {
-  stdio: "inherit",
-});
+
 console.log("2");
